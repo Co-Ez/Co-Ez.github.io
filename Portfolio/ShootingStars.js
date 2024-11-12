dd44abbb8cf2b3b58efd2303e2ced5dd7560ed22
Co-Ez <edwards.ezzie@gmail.com>
2024-11-12 10:53:39 +0000

    comments

--- Portfolio/ShootingStars.js
+++ Portfolio/ShootingStars.js
@@ -1,4 +1,4 @@
-class RandomCoordinates {
+class RandomCoordinates { // gives the element a random x and y position
     constructor() {
         this.x = Math.random() * 100 + '%';
         this.y = Math.random() * 100 + '%';
@@ -8,28 +8,28 @@ class RandomCoordinates {
 
 class ShootingStar {
     constructor() {
-        this.star = document.createElement('div');
-        this.star.classList.add('star');
+        this.star = document.createElement('div'); // create the element to be the star
+        this.star.classList.add('star'); // add the class of star to the element, makes it look like a star
 
-        this.start = new RandomCoordinates();
+        this.start = new RandomCoordinates(); // gives it a random starting position
 
-        this.setStyles();
+        this.setStyles(); // sets the look of the stars, size, angle, etc
     }
 
     setStyles() {
-        this.setNewStyleVar('--start-x', this.start.x);
-        this.setNewStyleVar('--start-y', this.start.y);
+        this.setNewStyleVar('--start-x', this.start.x); // applies starting position of the stars
+        this.setNewStyleVar('--start-y', this.start.y); //-------------------------------
 
-        this.setNewStyleVar('--duration', Math.random() * 6 + 5 + 's'); 
+        this.setNewStyleVar('--duration', Math.random() * 6 + 5 + 's'); //controls how long the stars last
 
-        this.setNewStyleVar('--delay', Math.random() * 0.2);
+        this.setNewStyleVar('--delay', Math.random() * 0.2); //controls delay of stars
 
-        this.setNewStyleVar('--top', Math.random() * 100 - 30 + '%');
-        this.setNewStyleVar('--left', Math.random() * 100 - 30+ '%');
+        this.setNewStyleVar('--top', Math.random() * 100 - 30 + '%'); // controls where they start from
+        this.setNewStyleVar('--left', Math.random() * 100 - 30+ '%'); // ------------------------------
 
-        this.setNewStyleVar('--size', Math.random() + 0.1);
+        this.setNewStyleVar('--size', Math.random() + 0.1); // controls the size of the stars
 
-        this.setNewStyleVar('--angle', Math.random() * 360 + 'deg');
+        this.setNewStyleVar('--angle', Math.random() * 360 + 'deg'); // controls the angle of the stars
     }
 
     setNewStyleVar(styleName, styleValue) {
@@ -37,7 +37,7 @@ class ShootingStar {
     }
 
     getStar() {
-        return this.star;
+        return this.star; // returns the element, to be pushed to the screen
     }
 }
 
@@ -45,17 +45,17 @@ new ShootingStar();
 
 class NightSky {
     constructor() {
-        this.nightSky = document.getElementById('night-sky');
+        this.nightSky = document.getElementById('night-sky'); // gets the element that will have the stars as children
 
         this.numberOfStars = Number.parseInt(this.nightSky.style.getPropertyValue('--number-of-stars'));
-        this.addStars(this.numberOfStars);
+        this.addStars(this.numberOfStars); // (also line above) gets grabs the number of stars wanted and then creates that many with the function
     }
 
     addStars(numberOfStars) {
-        for (let i = 0; i < numberOfStars; i++) {
-            this.nightSky.appendChild(new ShootingStar().getStar());
+        for (let i = 0; i < numberOfStars; i++) { //makes a new element/star everytime it goes through the list
+            this.nightSky.appendChild(new ShootingStar().getStar()); 
         }
     }
 }
 
-new NightSky();
+new NightSky(); // pushes it to the display screen
--- Portfolio/SiblingBlur.js
+++ Portfolio/SiblingBlur.js
@@ -1,7 +1,8 @@
 const cards = document.querySelectorAll('.cards, .cards-asso');
+// gets all the elements with the 'cards' class or 'cards-asso' class
 
-cards.forEach(card => {
-    card.addEventListener('mouseenter', () => {
+cards.forEach(card => { // runs through each element with the class of 'cards' or 'cards-asso'
+    card.addEventListener('mouseenter', () => { // adds an event listener for when the mouse enters the element
         cards.forEach(sibling => {
             if (sibling !== card) sibling.classList.add('blur');
         });
